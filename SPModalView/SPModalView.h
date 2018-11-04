717a9cecb17fafe00d15a23b2606305a621eeac4
Samarth Paboowal <samarthpaboowal@yahoo.co.in>
2018-11-04 16:00:52 +0530

    initial commit

---
+++ SPModalView/SPModalView.h
@@ -0,0 +1,19 @@
+//
+//  SPModalView.h
+//  SPModalView
+//
+//  Created by Samarth Paboowal on 04/11/18.
+//  Copyright © 2018 Samarth Paboowal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SPModalView.
+FOUNDATION_EXPORT double SPModalViewVersionNumber;
+
+//! Project version string for SPModalView.
+FOUNDATION_EXPORT const unsigned char SPModalViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPModalView/PublicHeader.h>
+
+
